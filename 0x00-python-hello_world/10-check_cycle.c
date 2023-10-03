fe5c3bd26e5a03bc1c436ee50e4f31d47f756a66
Ron David <rondavid209@gmail.com>
2023-10-03 06:34:15 +0300

    Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,31 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * check_cycle - checks if a singly linked list has a cycle in it.
- * @list: pointer to head of list.
- * Return: 0 if there is no cycle, 1 if there is a cycle.
+ * check_cycle - Checks if a singly-linked list contains a cycle.
+ * @list: A singly-linked list.
+ *
+ * Return: If there is no cycle - 0.
+ *         If there is a cycle - 1.
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *ptr1 = NULL;
-	listint_t *ptr2 = NULL;
+	listint_t *turtle, *hare;
 
-	if (list == NULL)
+	if (list == NULL || list->next == NULL)
 		return (0);
-	ptr1 = list;
-	ptr2 = list;
 
-	while (ptr1 && ptr2 && ptr2->next)
+	turtle = list->next;
+	hare = list->next->next;
+
+	while (turtle && hare && hare->next)
 	{
-		ptr1 = ptr1->next;
-		ptr2 = ptr2->next->next;
-
-		if (ptr1 == ptr2)
+		if (turtle == hare)
 			return (1);
+
+		turtle = turtle->next;
+		hare = hare->next->next;
 	}
+
 	return (0);
 }
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,27 +1,25 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
 
 /**
- * check_cycle - checks if a singly linked list has a cycle in it.
- * @list: pointer to head of list.
- * Return: 0 if there is no cycle, 1 if there is a cycle.
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
  */
-int check_cycle(listint_t *list)
+typedef struct listint_s
 {
-	listint_t *ptr1 = NULL;
-	listint_t *ptr2 = NULL;
+    int n;
+    struct listint_s *next;
+} listint_t;
 
-	if (list == NULL)
-		return (0);
-	ptr1 = list;
-	ptr2 = list;
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
-	while (ptr1 && ptr2 && ptr2->next)
-	{
-		ptr1 = ptr1->next;
-		ptr2 = ptr2->next->next;
-
-		if (ptr1 == ptr2)
-			return (1);
-	}
-	return (0);
-}
+#endif /* LISTS_H */
