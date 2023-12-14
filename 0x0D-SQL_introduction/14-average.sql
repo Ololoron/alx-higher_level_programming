116c44fcfa05303563e73cc5a2c2a52cefd89f74
Ron David <rondavid209@gmail.com>
2023-12-14 16:03:03 +0300

    Number by score

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- script 14
+-- Computes the score average of all records in the table second_table
+-- of the data_base htbn_0c_0 in your MySQL server
+
+SELECT AVG(score) AS average FROM second_table
